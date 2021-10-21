6d117a8c567bb23a1b93a9feb2989564a3a3480c
karim gomez <karim.gomez29@yahoo.com>
2021-10-21 12:36:57 -0400

    finished About js page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,21 @@ import React from "react";
 export default function About() {
     return (
         <div>
-            <h1>About page</h1>
+            <div className="px-4 py-5 my-5 text-center">
+                <img
+                    className="d-block mx-auto mb-4 profileImage"
+                    src="../assets/parrot.jpg"
+                    alt=""
+                />
+                <h1 className="display-5 fw-bold">About</h1>
+                <div className="col-lg-6 mx-auto">
+                    <p>
+                        I am a new Full Stack Developer still learning more and more about Technologies as the days go by. I'm interested in workout, fitness, technology and sports.
+                    </p>
+                </div>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                </div>
+            </div>
         </div>
-    )
+    );
 }
